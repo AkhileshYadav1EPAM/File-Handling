966d8731b60b53f8afa55adc95a15281d5fd6e32
Akhilesh Yadav1 <akhilesh_yadav1@epam.com>
2022-12-14 11:32:06 +0530

    first commit of file handling

---
+++ FileHanding/ReadingFromFile/ReadingFromFiles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingFromFile
+{
+    internal class ReadingFromFiles
+    {
+        static void Main(string[] args)
+        {
+            string folderPathForReading = "C:\\Users\\Akhilesh_Yadav1\\Desktop\\C# Example Codes\\2 - C# Examples\\LINQ Practice\\File Handling\\Files folder";
+            string fileNameForReading = "Reading.txt";
+            string filePathForReading = Path.Combine(folderPathForReading, fileNameForReading);
+
+            StreamReader sr = new StreamReader(filePathForReading);
+
+            Console.WriteLine("Content of the File");
+
+            string readingText = sr.ReadLine();
+            int numberOfLineInTheFile = 0;
+
+            while (readingText != null)
+            {
+                numberOfLineInTheFile++;
+                Console.WriteLine(readingText);
+                readingText = sr.ReadLine();
+            }
+
+            Console.WriteLine("The number of lines in the file is " + numberOfLineInTheFile);
+
+            Console.ReadLine();
+
+            sr.Close();
+        }
+    }
+}
